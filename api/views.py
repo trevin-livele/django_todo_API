f6505cf1a930b2c7fe7517da81f1b7866ae8c8c1
trevin-livele <liveletrevin6@gmail.com>
2022-04-04 11:19:51 +0300

    added sign up function

--- api/views.py
+++ api/views.py
@@ -3,9 +3,30 @@ from rest_framework import generics, permissions
 from .serializer import TodoSerializer,TodoCompleteSerializer
 from todo.models import Todo
 from django.utils import timezone
-
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser.parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            return JsonResponse({'token' : 'hjjdjdjdjdjdj'}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username'}, status=400)  
+    
+
+
+
+
+
+
+
 
 
 class TodoCompletedList(generics.ListAPIView):
